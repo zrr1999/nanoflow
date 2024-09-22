77ab58432ddfa7b942821ffeafa0b5f65ce7dafe
zrr1999 <2742392377@qq.com>
2024-09-22 23:09:06 +0800

    📝 docs: add examples

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import tempfile
-
 from typer.testing import CliRunner
 
 from nanoflow.__main__ import app
@@ -10,20 +8,5 @@ runner = CliRunner()
 
 
 def test_app():
-    with tempfile.NamedTemporaryFile(suffix=".toml", mode="w+t") as temp_config:
-        temp_config.write("""
-name = "test"
-
-[tasks.a]
-command = "echo 'a'"
-
-[tasks.b]
-command = "echo 'b'"
-
-[tasks.c]
-command = "echo 'c'"
-deps = ["a", "b"]
-""")
-        temp_config.flush()
-        result = runner.invoke(app, [temp_config.name])
-        assert result.exit_code == 0
+    result = runner.invoke(app, ["examples/simple.toml"])
+    assert result.exit_code == 0
